bfa86cc3efb2c0ff87752ba9aacb9a3dda56a691
Duncan Macleod <macleoddm@cardiff.ac.uk>
2026-09-20 10:57:39 +0100

    build: fix paths for cross-compiling

--- recipe/build-java.sh
+++ recipe/build-java.sh
@@ -10,19 +10,34 @@ cd _build_java
 # python for testing
 export PYTHON="${BUILD_PREFIX}/bin/python"
 
+# set up cross compilation args
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  LIB_JVM="${BUILD_PREFIX}/lib/jvm"
+  export CROSS_COMPILE_ARGS="
+    -DJAVA_INCLUDE_PATH='${LIB_JVM}/include'
+    -DJAVA_INCLUDE_PATH2='${LIB_JVM}/include/linux'
+    -DJAVA_AWT_INCLUDE_PATH='${LIB_JVM}/include'
+    -DJAVA_AWT_LIBRARY='${LIB_JVM}/lib/libjawt.so'
+    -DJAVA_JVM_LIBRARY='${LIB_JVM}/lib/server/libjvm.so'
+    -DCMAKE_C_FLAGS='-I${LIB_JVM}/include -I${LIB_JVM}/include/linux'
+    -DCMAKE_CXX_FLAGS='-I${LIB_JVM}/include -I${LIB_JVM}/include/linux'
+  "
+fi
+
 # configure
 cmake \
-	${SRC_DIR} \
-	${CMAKE_ARGS} \
-	-DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
-	-DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=yes \
-	-DCMAKE_POLICY_VERSION_MINIMUM:STRING=3.5 \
-	-DENABLE_SWIG_JAVA:BOOL=yes \
-	-DENABLE_SWIG_MATLAB:BOOL=no \
-	-DENABLE_SWIG_OCTAVE:BOOL=no \
-	-DENABLE_SWIG_PYTHON2:BOOL=no \
-	-DENABLE_SWIG_PYTHON3:BOOL=no \
-	-DSWIG_EXECUTABLE:PATH="${BUILD_PREFIX}/bin/swig" \
+  ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  ${CROSS_COMPILE_ARGS} \
+  -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
+  -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=yes \
+  -DCMAKE_POLICY_VERSION_MINIMUM:STRING=3.5 \
+  -DENABLE_SWIG_JAVA:BOOL=yes \
+  -DENABLE_SWIG_MATLAB:BOOL=no \
+  -DENABLE_SWIG_OCTAVE:BOOL=no \
+  -DENABLE_SWIG_PYTHON2:BOOL=no \
+  -DENABLE_SWIG_PYTHON3:BOOL=no \
+  -DSWIG_EXECUTABLE:PATH="${BUILD_PREFIX}/bin/swig" \
 ;
 
 # build
